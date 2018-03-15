47dfdbadf1763f90fa2e8a36eb523aa22586ef2f
Lappy Mc Lap Face <ephillipsiii>
2018-03-15 15:24:44 -0600

    working on response from API

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -2,27 +2,27 @@
 var videoGames = ["Skyrim", "Dark Souls", "Dark Souls 2", "Dark Souls 3", "Bloodborne", "The Witcher 3", "World of Warcraft"];
 
 function videoGameGif() {
-    var videoGame = $(this).attr('data-name');
-    var gueryURL = "https://api.gihpy.com/v1/gifs/search?q=" + videoGames + "&api+key=Vflj615zpDEH41Qqr3XVVlGGVq7HSlzZ&limit=10";
+    var videoGame = $(this).attr("data-name");
+    var gueryURL = "https://api.giphy.com/v1/gifs/search?api_key=Vflj615zpDEH41Qqr3XVVlGGVq7HSlzZ&q=" + videoGame + "&limit=10&offset=0&rating=R&lang=en";
 
     $.ajax({
-        url:queryURl,
+        url: queryURl,
         method: "GET"
-        // }).then(function(response){
-        //     var vgDiv = $("<div class='videoGame'>");
-        //     var rating = response.rated;
+        }).then(function(response){
+            var vgDiv = $("<div class='videoGame'>");
+            var rated = response.rating;
         })
-    console.log(videoGameGif);
+    console.log(videoGame);
 }
 
 //rendering the buttons
-function renderButons() {
-    $("videoGame-button").empty();
-    for (var i=0; i < videoGames.length, i++;) {
-        var a = $("<button>");
-        a.addClass("vG");
-        a.attr("data-name", videoGames[i]);
-        a.text(videGames[i]);
-        $("videoGame-buttons").append(a);
-    }
-}
+// function renderButons() {
+//     $("videoGame-button").empty();
+//     for (var i=0; i < videoGames.length, i++;) {
+//         var a = $("<button>");
+//         a.addClass("vG");
+//         a.attr("data-name", videoGames[i]);
+//         a.text(videGames[i]);
+//         $("videoGame-buttons").append(a);
+//     }
+// }
