e32368f41a4fce6c2579b48648e7bc2556ee6fd6
Lappy Mc Lap Face <ephillipsiii>
2018-03-16 17:43:03 -0600

    button array working!

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,28 +1,50 @@
+$(document).ready(function(){
 //initial array of video games
-var videoGames = ["Skyrim", "Dark Souls", "Dark Souls 2", "Dark Souls 3", "Bloodborne", "The Witcher 3", "World of Warcraft"];
+    var videoGames = ["Skyrim", "Dark Souls", "Dark Souls 2", "Dark Souls 3", "Bloodborne", "The Witcher 3", "World of Warcraft"];
+    console.log(videoGames);
 
-function videoGameGif() {
-    var videoGame = $(this).attr("data-name");
-    var gueryURL = "https://api.giphy.com/v1/gifs/search?api_key=Vflj615zpDEH41Qqr3XVVlGGVq7HSlzZ&q=" + videoGame + "&limit=10&offset=0&rating=R&lang=en";
+    // rendering the buttons
+    function renderButtons() {
+        $("#buttons-view").empty();
+        for (var i = 0; i < videoGames.length; i++) {
+            var a = $("<button>");
+            a.addClass("videoGame-btn");
+            a.attr("data-name", videoGames[i]);
+            a.text(videoGames[i]);
+            $("#buttons-view").append(a);
+        }
+    }
 
-    $.ajax({
-        url: queryURl,
-        method: "GET"
-        }).then(function(response){
-            var vgDiv = $("<div class='videoGame'>");
-            var rated = response.rating;
-        })
-    console.log(videoGame);
-}
 
-//rendering the buttons
-// function renderButons() {
-//     $("videoGame-button").empty();
-//     for (var i=0; i < videoGames.length, i++;) {
-//         var a = $("<button>");
-//         a.addClass("vG");
-//         a.attr("data-name", videoGames[i]);
-//         a.text(videGames[i]);
-//         $("videoGame-buttons").append(a);
-//     }
-// }
+    function videoGameGif() {
+        var videoGame = $(this).attr("data-name");
+    //     var gueryURL = "https://api.giphy.com/v1/gifs/search?q=" + videoGame + "&api_key=Vflj615zpDEH41Qqr3XVVlGGVq7HSlzZ&limit=10&offset=0&rating=R&lang=en";
+
+    // $.ajax({
+    //     url: queryURl,
+    //     method: "GET"
+    //     })
+    //     .then(function(response){
+    //         console.log(response);
+        // var vgDiv = $("<div class='videoGame'>");
+        // var rating = response.Rating;
+        // var pOne = $('<p>').text('Rating: ' + rating);
+        // vgDiv.append(pOne);
+        // var imgURL = response.Gif;
+        // var image = $("<img>").attr("src",imgURL);
+        // vgDiv.append(image);
+        // })
+    
+    };
+
+
+    $("#addVG").on("click", function(event) {
+        event.preventDefault();
+        var videoGame = $("#videoGame-input").val().trim();
+        videoGames.push(videoGame);
+        renderButtons();
+    });
+    $(document).on("click'", ".videoGame-btn", videoGameGif);
+    renderButtons();
+});    
+
