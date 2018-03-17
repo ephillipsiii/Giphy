93129c37733d75135bd3f37c30f4bf9c8c0b608f
Lappy Mc Lap Face <ephillipsiii>
2018-03-17 13:48:37 -0600

    working ajax response, working buttons, still need to work on the onclick pausing/starting for gifs

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,8 +1,6 @@
-$(document).ready(function(){
-//initial array of video games
+$(document).ready(function () {
+    //initial array of video games
     var videoGames = ["Skyrim", "Dark Souls", "Dark Souls 2", "Dark Souls 3", "Bloodborne", "The Witcher 3", "World of Warcraft"];
-    console.log(videoGames);
-
     // rendering the buttons
     function renderButtons() {
         $("#buttons-view").empty();
@@ -14,37 +12,36 @@ $(document).ready(function(){
             $("#buttons-view").append(a);
         }
     }
-
-
-    function videoGameGif() {
-        var videoGame = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + videoGame + "&api_key=Vflj615zpDEH41Qqr3XVVlGGVq7HSlzZ&limit=1";
-console.log(queryURL);
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-        })
-        // .then(function(response){
-            
-        // var vgDiv = $("<div class='videoGame'>");
-        // var rating = response.Rating;
-        // var pOne = $('<p>').text('Rating: ' + rating);
-        // vgDiv.append(pOne);
-        // var imgURL = response.Gif;
-        // var image = $("<img>").attr("src",imgURL);
-        // vgDiv.append(image);
-        // })
-    
-    // });
-    // console.log(response);
-    };
-    $("#addVG").on("click", function(event) {
+    $("#addVG").on("click", function (event) {
         event.preventDefault();
         var videoGame = $("#videoGame-input").val().trim();
         videoGames.push(videoGame);
         renderButtons();
     });
-    $(document).on("click'", ".videoGame-btn", videoGameGif);
+    $(document).on("click", ".videoGame-btn", function () {
+        var videoGame = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + videoGame + "&api_key=Vflj615zpDEH41Qqr3XVVlGGVq7HSlzZ&limit=10";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .then(function (response) {
+            var results = response.data
+                for (i = 0; i < results.length; i++) {
+                    var vgDiv = $("<div>");
+                    var rating = results[i].rating;
+                    var pOne = $('<p>').text('Rating: ' + rating);
+                    vgDiv.append(pOne);
+                    var imgURL = results[i].images.original.url;
+                    var image = $("<img>").attr("src", imgURL);
+                    vgDiv.prepend(image);
+                    $("#vG").prepend(vgDiv);
+                }
+        })        
+    });
     renderButtons();
+});
 
-});   
+
+
+
